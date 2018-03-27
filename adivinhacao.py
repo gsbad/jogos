492ae4ccc0437446e90c129d47bce32d5f718329
Gustavo Sá <gsbadbr@gmail.com>
2018-03-27 13:48:28 -0300

    Versão rebuscada do joguinho

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,23 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual nivel de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+elif(nivel == 3):
+    total_de_tentativas = 5
 
 
-print('teste')
+print(numero_secreto)
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
@@ -25,12 +38,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor   = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou! E fez {} pontos".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
+        punicao = abs(numero_secreto - chute)
+        pontos = pontos - punicao
 
 print("Fim do jogo")
